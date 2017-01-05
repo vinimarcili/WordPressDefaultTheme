04f903f32a7da8477f1aea1d7d163ac0396df391
Vinícius Marcili <vinipetter@gmail.com>
2017-01-05 10:55:13 -0200

    Adicionado pagina 404

---
+++ theme_luapp/404.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package WordPress
+ * @subpackage Theme_Luapp
+ * @since Theme 1.0
+ */
+
+get_header(); ?>
+
+    <main id="page-404">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h1><span>404 - Página não encontrada</span></h1>
+                    <p>Ops! A página que você procura não existe :(</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
